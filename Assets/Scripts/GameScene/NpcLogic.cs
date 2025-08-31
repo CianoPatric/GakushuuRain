91aaa366dc9b4d1c5ef4cc733b1312e3f14514ea
DESKTOP-CDOD8P1\nikit <nikitka.kopylov.2018@bk.ru>
2025-08-31 17:37:13 +0400

    Добавил всю графику, поставил сцену, доработал систему показа диалогов

--- Assets/Scripts/GameScene/NpcLogic.cs
+++ Assets/Scripts/GameScene/NpcLogic.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 
@@ -5,21 +6,30 @@ public class NpcLogic : MonoBehaviour
 {
     private Transform body;
     private DialogueManager DialogueWindow;
+    public GameObject PressEWindow;
     
     public void OnTriggerEnter2D(Collider2D other)
     {
-        body = transform;
-        var window = Resources.Load<DialogueManager>("DialogueWindow");
-        DialogueWindow = Instantiate(window, body.position, Quaternion.identity);
-        DontDestroyOnLoad(DialogueWindow.gameObject);
-        DialogueWindow.transform.SetParent(body, false);
-        DialogueWindow.transform.position = body.position + new Vector3(-150, 180, 0);
-        var text = DialogueWindow.GetComponentInChildren<TextMeshProUGUI>();
-        DialogueWindow.ShowDialogueLine("{object_field} {creature_canis}");
+        PressEWindow.SetActive(true);
+    }
+
+    public void OnTriggerStay2D(Collider2D other)
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            body = transform;
+            var window = Resources.Load<DialogueManager>("DialogueWindow");
+            DialogueWindow = Instantiate(window, body.position, Quaternion.identity);
+            DontDestroyOnLoad(DialogueWindow.gameObject);
+            DialogueWindow.transform.SetParent(body, false);
+            DialogueWindow.transform.position = body.position + new Vector3(-150, 180, 0);
+            var text = DialogueWindow.GetComponentInChildren<TextMeshProUGUI>();
+            DialogueWindow.ShowDialogueLine("{object_field} {creature_canis}");
+        }
     }
 
     public void OnTriggerExit2D(Collider2D other)
     {
-        Destroy(DialogueWindow.gameObject);
+        PressEWindow.SetActive(false);
     }
 }
--- Assets/Scripts/GameScene/PlayerMovement.cs
+++ Assets/Scripts/GameScene/PlayerMovement.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float moveSpeed = 250f;
     
+    public Animator animator;
     private Rigidbody2D rb;
     private Vector2 movement;
 
@@ -15,11 +16,23 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
+        if (movement.x < 0)
+        {
+            this.transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else
+        {
+            if(movement.x > 0)
+            {
+                this.transform.localScale = new Vector3(1, 1, 1);
+            }
+        }
         movement.y = Input.GetAxisRaw("Vertical");
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+        animator.SetFloat("Speed", movement.magnitude);
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime); 
     }
 }
